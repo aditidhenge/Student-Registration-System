ef0ba7252e13c1f7bdf036098d17702f5d3b2100
AditiDhenge <103129934+aditidhenge@users.noreply.github.com>
2023-07-21 20:04:18 +0530

    Update tree structure.cpp

--- tree structure.cpp
+++ tree structure.cpp
@@ -5,23 +5,24 @@ typedef struct course{
     int id;
     char name[50];
     int slot;
-  	struct stunode* sroot;
+    struct stunode* sroot; //root of the bst of students enrolled in this course
     struct course *left;
     struct course *right;
-    int height;
-    int count;
-    int classroom; 
+    int height; //height of the AVL Tree
+    int count;  //number of students enrolled in this course
+    int classroom; //classroom number where the course is held
 }course;
 
-//for student in course database
+
 struct course_student{
-	int benchno;
-	int rowno;
-	int classroom;
+	int benchno;  //bench number in the classroom
+	int rowno;    //row number in the classroom
+	int classroom; 
 	int enrolment_number;
-    char name[50];    
+	char name[50];    
 };
 
+//structure to represent students in a specific course
 typedef struct stunode {
     struct course_student student;
     int height;
@@ -33,8 +34,8 @@ typedef struct stunode {
 typedef struct student {
     int enrolment_number;
     char name[50];
-    int courses[5];
-} Student;
+    int courses[5];  //to store course ids of the courses the student is enrolled in
+}Student;
 
 typedef struct node {
     Student student;
@@ -44,12 +45,11 @@ typedef struct node {
 }Node;
 
 //for classroom database
-
 typedef struct Classroom 
 {
     int roomNumber;
-    int capacity;
-} Classroom;
+    int capacity; //maximum capacity of classroom
+}Classroom;
 
 typedef struct AVLNode 
 {
@@ -57,4 +57,4 @@ typedef struct AVLNode
     struct AVLNode* left;
     struct AVLNode* right;
     int height;
-} AVLNode;
+}AVLNode;

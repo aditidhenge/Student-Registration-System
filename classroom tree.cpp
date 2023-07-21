210e1ecb3b51fca0ae34606a0b8d69c13a80a22a
AditiDhenge <103129934+aditidhenge@users.noreply.github.com>
2023-07-21 21:14:02 +0530

    Update classroom tree.cpp

--- classroom tree.cpp
+++ classroom tree.cpp
@@ -15,7 +15,6 @@ int maxCL(int a, int b)
     return (a > b) ? a : b;
 }
 
-// Function to get the height of an AVL tree node
 int getHeightCL(AVLNode* node) 
 {
     if (node == NULL) {
@@ -24,7 +23,6 @@ int getHeightCL(AVLNode* node)
     return node->height;
 }
 
-// Function to get the balance factor of an AVL tree node
 int getBalanceCL(AVLNode* node) 
 {
     if (node == NULL) {
@@ -33,7 +31,6 @@ int getBalanceCL(AVLNode* node)
     return getHeightCL(node->left) - getHeightCL(node->right);
 }
 
-// Function to rotate an AVL tree node to the left
 AVLNode* rotateLeftCL(AVLNode* node) 
 {
     AVLNode* newRoot = node->right;
@@ -45,7 +42,6 @@ AVLNode* rotateLeftCL(AVLNode* node)
     return newRoot;
 }
 
-// Function to rotate an AVL tree node to the right
 AVLNode* rotateRightCL(AVLNode* node) 
 {
     AVLNode* newRoot = node->left;
@@ -57,10 +53,8 @@ AVLNode* rotateRightCL(AVLNode* node)
     return newRoot;
 }
 
-// Function to insert a new node into an AVL tree
 AVLNode* insertCL(AVLNode* node, Classroom classroom) 
 {
-    // Perform standard BST insertion
     if (node == NULL) 
     {
         return newNodeCL(classroom);
@@ -75,46 +69,36 @@ AVLNode* insertCL(AVLNode* node, Classroom classroom)
     } 
     else 
     {
-        // If the classroom already exists, update the capacity
         node->classroom.capacity = classroom.capacity;
         return node;
     }
     
-    // Update the height of the current node
     node->height = 1 + maxCL(getHeightCL(node->left), getHeightCL(node->right));
     
-    // Get the balance factor of the current node
     int balance = getBalanceCL(node);
     
-    // If the node becomes unbalanced, there are four cases
-    // Left Left Case
     if (balance > 1 && classroom.capacity < node->left->classroom.capacity) 
     {
         return rotateRightCL(node);
     }
-    // Right Right Case
     if (balance < -1 && classroom.capacity > node->right->classroom.capacity) 
     {
     return rotateLeftCL(node);
     }
-    // Left Right Case
     if (balance > 1 && classroom.capacity > node->left->classroom.capacity) 
     {
         node->left = rotateLeftCL(node->left);
         return rotateRightCL(node);
     }
-    // Right Left Case
     if (balance < -1 && classroom.capacity < node->right->classroom.capacity) 
     {
         node->right = rotateRightCL(node->right);
         return rotateLeftCL(node);
     }
 
-    // Return the updated node
     return node;
 }
 
-// Function to print an AVL tree in-order
 void printInOrder(AVLNode* node) 
 {
     if (node == NULL) 
